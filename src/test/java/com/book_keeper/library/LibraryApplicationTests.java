1abe1cf6c17d261c76f0991c8c789b35c4b718de
Ini Davies <inidavies24@gmail.com>
2025-05-24 19:36:11 -0500

    project start

---
+++ src/test/java/com/book_keeper/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_keeper.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
